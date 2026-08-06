f75d9cc120b96b0d655aca13fc8d4a514362718f
Xtrimisyt <151603859+Xtrimisyt@users.noreply.github.com>
2026-08-06 07:42:32 +0530

    Update CommandProcessor.kt

--- app/src/main/java/com/brosco/assistant/CommandProcessor.kt
+++ app/src/main/java/com/brosco/assistant/CommandProcessor.kt
@@ -33,8 +33,8 @@ object CommandProcessor {
         "telegram" to "org.telegram.messenger",
         "prime video" to "com.amazon.avod.thirdpartyclient",
         "zomato" to "com.application.zomato",
-        "dominos" to "pizzaonline.dominos",
-        "domino's" to "pizzaonline.dominos"
+        "dominos" to "com.Dominos",
+        "domino's" to "com.Dominos"
     )
 
     fun process(context: Context, rawText: String, scope: CoroutineScope, speak: (String) -> Unit) {
